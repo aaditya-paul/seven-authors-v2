89a44c22dcf7ba8ed8aaa2f520553e25a3c5685f
Rajdeep Karmakar <rajdeepkarmakar2027@gmail.com>
2024-10-08 16:13:34 +0530

    book slider, faq and footer added

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,47 @@
+"use client";
 import NavBar from "@/components/navBar";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import RedLogo from "@/../public/assets/img/logo-red.svg";
+
+import faqimg from "@/../public/assets/img/faqImg.png";
+
+import wavyBg from "/public/assets/wavybg.svg";
+
+import BookSLider from "@/components/BookSLider";
+import Faq from "@/components/FaqQuestions";
+import FaqQuestions from "@/components/FaqQuestions";
+import { faQ } from "@fortawesome/free-solid-svg-icons";
 
 function Page() {
+  const faqArray = [
+    {
+      title: "Tailwind faq section with border layout?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex quaerat labore accusamus dolorum similique eveniet natus numquam vitae eos dignissimos?",
+    },
+    {
+      title: "Tailwind faq section with border layout?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex quaerat labore accusamus dolorum similique eveniet natus numquam vitae eos dignissimos?",
+    },
+    {
+      title: "Tailwind faq section with border layout?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex quaerat labore accusamus dolorum similique eveniet natus numquam vitae eos dignissimos?",
+    },
+    {
+      title: "Tailwind faq section with border layout?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex quaerat labore accusamus dolorum similique eveniet natus numquam vitae eos dignissimos?",
+    },
+    {
+      title: "Tailwind faq section with border layout?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex quaerat labore accusamus dolorum similique eveniet natus numquam vitae eos dignissimos?",
+    },
+  ];
   return (
     <div className=" overflow-hidden">
       <NavBar />
@@ -27,6 +66,85 @@ function Page() {
                 Sign In
               </Link>
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#222222]">
+        <div className="bg-wavyBG p-3 bg-repeat text-white flex flex-col justify-center items-center">
+          <div className="bg-[#222222] lg:flex justify-between lg:mx-40 mt-10 lg:px-10 px-5  gap-10 lg:py-5 py-5  rounded-lg">
+            <BookSLider />
+          </div>
+          <div className="bg-[#222222] mx-40 mt-10 px-10 py-5 rounded-lg">
+            <h1 className="text-2xl my-4">Frequently asked questions</h1>
+            <div className="flex justify-center items-center gap-10">
+              <div className="flex flex-col gap-4">
+                {faqArray.map((faq, index) => {
+                  return (
+                    <>
+                      <FaqQuestions ques={faq} index={index} />
+                    </>
+                  );
+                })}
+              </div>
+              <div className="image hidden md:flex">
+                <Image src={faqimg} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class=" text-white flex bg-[#222222] justify-center">
+        <div class="grid grid-cols-1 md:grid-cols-4 md:w-[120ch] p-[24px] gap-[32px] ">
+          <div class="flex flex-col gap-[16px]">
+            <Link href="/">
+              <Image src={RedLogo} alt="logo"></Image>
+            </Link>
+            <p class=" text-sm ">
+              In a post-apocalyptic world where books have been banned and
+              burned, a young woman named Anya discovers a hidden library.
+            </p>
+          </div>
+          <div class="flex flex-col gap-[16px]">
+            <p class="font-bold text-[16px]">Service</p>
+            <div class="flex text-[12px]  flex-col gap-[12px]">
+              <Link href={"/"} class="hover:text-[#E12F3B] hover:underline">
+                Home
+              </Link>
+              <Link href={"/"} class="hover:text-[#E12F3B] hover:underline">
+                E-book
+              </Link>
+              <Link href={"/"} class="hover:text-[#E12F3B] hover:underline">
+                Audio book
+              </Link>
+              <Link href={"/"} class="hover:text-[#E12F3B] hover:underline">
+                partner
+              </Link>
+            </div>
+          </div>
+          <div class="flex flex-col gap-[16px]">
+            <p class="font-bold text-[16px]">.</p>
+            <div class="flex text-[12px]  flex-col gap-[12px]">
+              <Link href={"/"} class="hover:text-[#E12F3B] hover:underline">
+                Terms & Service
+              </Link>
+              <Link href={"/"} class="hover:text-[#E12F3B] hover:underline">
+                Privacy Policy
+              </Link>
+              <Link href={"/"} class="hover:text-[#E12F3B] hover:underline">
+                Return Policy
+              </Link>
+            </div>
+          </div>
+          <div class="flex flex-col gap-[16px]">
+            <p class="font-bold text-[16px]">Comapny</p>
+            <div class="flex text-[12px]  flex-col gap-[12px]">
+              <Link href={"/"} class="hover:text-[#E12F3B] hover:underline">
+                About Us
+              </Link>
+              <Link href={"/"} class="hover:text-[#E12F3B] hover:underline">
+                Contact us
+              </Link>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/FaqQuestions.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+const FaqQuestions = ({ ques, index }) => {
+  const [isquesOpen, setIsquesOpen] = useState(false);
+
+  console.log(index, ques)
+
+  return (
+    <>
+      <div
+        key={index}
+        className={` ${
+          isquesOpen ? "bg-[#E12F3B]/10 border-red-700" : "bg-white/10 border-gray-500  "
+        } lg:w-[700px] w-[350px] border-2 transition-all duration-300 ease-in-out cursor-pointer p-3 rounded-lg`}
+        onClick={() => {
+          setIsquesOpen(!isquesOpen);
+        }}
+      >
+        <div className={`${isquesOpen ? "text-red-500" : "text-white"}`}>
+          {ques.title}
+        </div>
+        {isquesOpen && <div className="text-white">{ques.answer}</div>}
+      </div>
+    </>
+  );
+};
+
+export default FaqQuestions;
